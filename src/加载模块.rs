f769a1dfbca700f1bb3d075bddfdb88cbb893881
Erasin <erasinoo@gmail.com>
2021-07-23 15:26:53 +0800

    bevy_asset_loader

--- src/加载模块.rs
+++ src/加载模块.rs
@@ -2,6 +2,7 @@ use crate::全局状态;
 
 use bevy::asset::LoadState;
 use bevy::prelude::*;
+use bevy_asset_loader::{AssetCollection, AssetLoader};
 use bevy_kira_audio::AudioSource;
 
 pub struct AssetPaths {
@@ -12,6 +13,7 @@ pub struct AssetPaths {
     pub texture_box_blue: &'static str,
 }
 
+#[allow(dead_code)]
 pub const PATHS: AssetPaths = AssetPaths {
     font_ui: "fonts/KenneyFuture.ttf",
     audio_wall: "sounds/wall.wav",
@@ -24,8 +26,15 @@ pub struct 加载素材库插件;
 
 impl Plugin for 加载素材库插件 {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_system_set(SystemSet::on_enter(全局状态::加载中).with_system(开始加载.system()))
-            .add_system_set(SystemSet::on_update(全局状态::加载中).with_system(检查变动.system()));
+        // app.add_system_set(SystemSet::on_enter(全局状态::加载中).with_system(开始加载.system()))
+        // .add_system_set(SystemSet::on_update(全局状态::加载中).with_system(检查变动.system()));
+
+        AssetLoader::new(全局状态::加载中, 全局状态::菜单)
+            .with_collection::<字体素材>()
+            .with_collection::<音频素材>()
+            .with_collection::<纹理素材图片>()
+            .init_resource::<纹理素材>()
+            .build(app);
     }
 }
 
@@ -35,12 +44,26 @@ pub struct LoadingState {
     音频: Vec<HandleUntyped>,
 }
 
+#[derive(AssetCollection)]
 pub struct 字体素材 {
+    #[asset(path = "fonts/KenneyFuture.ttf")]
     pub font_ui: Handle<Font>,
 }
 
+#[derive(AssetCollection)]
 pub struct 音频素材 {
+    #[asset(path = "sounds/wall.wav")]
     pub audio_wall: Handle<AudioSource>,
+}
+
+#[derive(AssetCollection)]
+pub struct 纹理素材图片 {
+    #[asset(path = "textures/sheet.png")]
+    pub 纹理表: Handle<Texture>,
+    #[asset(path = "textures/player_sheet.png")]
+    pub 用户: Handle<Texture>,
+    #[asset(path = "textures/box_blue_sheet.png")]
+    pub 蓝箱子: Handle<Texture>,
 }
 
 pub struct 纹理素材 {
@@ -49,6 +72,39 @@ pub struct 纹理素材 {
     pub 蓝箱子: Handle<TextureAtlas>,
 }
 
+impl FromWorld for 纹理素材 {
+    fn from_world(world: &mut World) -> Self {
+        let cell = world.cell();
+        let assets = cell
+            .get_resource::<纹理素材图片>()
+            .expect("SpriteSheet not loaded");
+        let mut atlases = cell
+            .get_resource_mut::<Assets<TextureAtlas>>()
+            .expect("TextureAtlases missing");
+        纹理素材 {
+            纹理表: atlases.add(TextureAtlas::from_grid(
+                assets.纹理表.clone(),
+                Vec2::new(32., 32.),
+                5,
+                1,
+            )),
+            用户: atlases.add(TextureAtlas::from_grid(
+                assets.用户.clone(),
+                Vec2::new(32., 32.),
+                3,
+                1,
+            )),
+            蓝箱子: atlases.add(TextureAtlas::from_grid(
+                assets.蓝箱子.clone(),
+                Vec2::new(32., 32.),
+                2,
+                1,
+            )),
+        }
+    }
+}
+
+#[allow(dead_code)]
 fn 开始加载(mut commands: Commands, asset_server: Res<AssetServer>) {
     let mut fonts: Vec<HandleUntyped> = vec![];
     fonts.push(asset_server.load_untyped(PATHS.font_ui));
@@ -68,6 +124,7 @@ fn 开始加载(mut commands: Commands, asset_server: Res<AssetServer>) {
     });
 }
 
+#[allow(dead_code)]
 fn 检查变动(
     mut commands: Commands,
     mut state: ResMut<State<全局状态>>,
